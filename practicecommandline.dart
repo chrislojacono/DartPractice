355f24a75953236a95d16cff74661a09ce29ea8c
Chris LoJacono <clojacono92@gmail.com>
2021-07-11 09:52:27 -0500

    lessthan5

--- practicecommandline.dart
+++ practicecommandline.dart
@@ -29,3 +29,14 @@ void main() {
 }
 
 //write a program that prints out all the elements of the list that are less than 5.
+import 'dart:io';
+
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  for(var num in a) {
+    if(num < 5){
+      print(num);
+    }
+  }
+}
