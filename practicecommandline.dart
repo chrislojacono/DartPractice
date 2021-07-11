8c45f7f57317eb988c123950676dcdbd00541b82
Chris LoJacono <clojacono92@gmail.com>
2021-07-11 10:04:15 -0500

    save

--- practicecommandline.dart
+++ practicecommandline.dart
@@ -40,3 +40,38 @@ void main() {
     }
   }
 }
+
+// //Take two lists, for example:
+
+//   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+//   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// and write a program that returns a list that contains only the elements that are common between them (without duplicates). Make sure your program works on two lists of different sizes.
+
+import 'dart:io';
+
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  void ListSorter(List<int> listOne, List<int> listTwo){
+  List<int> finalList = new List<int>();
+    if(listOne.length > listTwo.length){
+      for(var num in listTwo){
+        if(listOne.contains(num)){
+         finalList.add(num);
+        }
+      }
+    }else{
+    for(var num in listOne){
+        if(listTwo.contains(num)){
+         finalList.add(num);
+        }
+      }
+    }
+    print(finalList);
+  }
+
+  ListSorter(a, b);
+}
