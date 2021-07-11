bd072529979726e3d6a31e8be5b05b084dec767e
Chris LoJacono <clojacono92@gmail.com>
2021-07-11 09:47:33 -0500

    odd or even

--- practicecommandline.dart
+++ practicecommandline.dart
@@ -10,3 +10,20 @@ print('How old are you?');
 }
 
 // Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+import 'dart:io';
+
+void main() {
+  stdout.write("Give me a number: ");
+
+  int number = int.parse(stdin.readLineSync());
+
+  void OddOrEven(int num){
+  if (num % 2 == 0){
+    stdout.write("$num is even");
+    } else{
+    stdout.write("$num is odd");
+    }
+  }
+
+  OddOrEven(number);
+}
